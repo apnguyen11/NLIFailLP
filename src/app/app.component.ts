f21bc6e9727bc4052afefd76b50b5b779c4c85d7
Andy Nguyen <andy.nguyen@aquila-system.com>
2020-08-03 14:27:59 -0500

    time change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -54,7 +54,7 @@ export class AppComponent {
 
 
   ngOnInit() {
-    this.sub = Observable.interval(425)
+    this.sub = Observable.interval(470)
     .subscribe((val) => {
       for(let i = 0; i < this.index; i++){
         this.LPData.push(this.XLPData[i])
